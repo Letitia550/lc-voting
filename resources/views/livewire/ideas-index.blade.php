ba1a705d0f7a329e741d5fe3f1732a855fe4a77a
Letitia550 <letitia.negotin99@e-uvt.ro>
2021-11-25 13:14:52 +0200

    Episode 25 - Status Filters Count

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -37,6 +37,8 @@
     </div> <!-- end ideas-container -->
 
     <div class="my-8">
-        {{ $ideas->links() }}
-    </div>
+        {{--{{ $ideas->links() }} --}}
+        <!-- pentru paginare dupa status -->
+        {{ $ideas->appends(request()->query())->links() }}
+</div>
 </div>
--- resources/views/livewire/status-filters.blade.php
+++ resources/views/livewire/status-filters.blade.php
@@ -1,12 +1,10 @@
 <nav class="hidden md:flex items-center justify-between text-xs text-gray-400">
     <ul class="flex uppercase font-semibold space-x-10 border-b-4 pb-3">
-        <li><a wire:click.prevent="setStatus('All')" href="#" class="border-b-4 pb-3 hover:border-blue @if ($status === 'All')border-blue text-gray-900 @endif">All Ideas (87)</a></li>
-        <li><a wire:click.prevent="setStatus('Considering')" href="#" class="transition ease-in duration-150 border-b-4 pb-3 hover:border-blue @if ($status === 'Considering')border-blue text-gray-900 @endif">Considering (6)</a></li>
-        <li><a wire:click.prevent="setStatus('In Progress')" href="#" class="transition ease-in duration-150 border-b-4 pb-3 hover:border-blue @if ($status === 'In Progress')border-blue text-gray-900 @endif">In Progress (1)</a></li>
-    </ul>
-
-    <ul class="flex uppercase font-semibold space-x-10 border-b-4 pb-3">
-        <li><a wire:click.prevent="setStatus('Implemented')" href="#" class="transition ease-in duration-150 border-b-4 pb-3 hover:border-blue @if ($status === 'Implemented')border-blue text-gray-900 @endif">Implemented (10)</a></li>
-        <li><a wire:click.prevent="setStatus('Closed')" href="#" class="transition ease-in duration-150 border-b-4 pb-3 hover:border-blue @if ($status === 'Closed')border-blue text-gray-900 @endif">Closed (56)</a></li>
+        <li><a wire:click.prevent="setStatus('All')" href="#" class="border-b-4 pb-3 hover:border-blue @if ($status === 'All')border-blue text-gray-900 @endif">All Ideas {{ $statusCount['all_statuses'] }}</a></li>
+        <li><a wire:click.prevent="setStatus('Considering')" href="#" class="transition ease-in duration-150 border-b-4 pb-3 hover:border-blue @if ($status === 'Considering')border-blue text-gray-900 @endif">Considering {{ $statusCount['considering'] }}</a></li>
+        <li><a wire:click.prevent="setStatus('In Progress')" href="#" class="transition ease-in duration-150 border-b-4 pb-3 hover:border-blue @if ($status === 'In Progress')border-blue text-gray-900 @endif">In Progress {{ $statusCount['in_progress'] }}</a></li>
+        <li><a wire:click.prevent="setStatus('Open')" href="#" class="transition ease-in duration-150 border-b-4 pb-3 hover:border-blue @if ($status === 'Open')border-blue text-gray-900 @endif">Open {{ $statusCount['open'] }}</a></li>
+        <li><a wire:click.prevent="setStatus('Implemented')" href="#" class="transition ease-in duration-150 border-b-4 pb-3 hover:border-blue @if ($status === 'Implemented')border-blue text-gray-900 @endif">Implemented {{ $statusCount['implemented'] }}</a></li>
+        <li><a wire:click.prevent="setStatus('Closed')" href="#" class="transition ease-in duration-150 border-b-4 pb-3 hover:border-blue @if ($status === 'Closed')border-blue text-gray-900 @endif">Closed {{$statusCount['closed'] }}</a></li>
     </ul>
 </nav>
