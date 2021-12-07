e6fa39953169509b9f4fb5d218489ea1419e572a
Letitia550 <negotin.letitia@gmail.com>
2021-12-07 15:19:39 +0200

    Episode 27 - Category Filters

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -9,21 +9,17 @@ use Livewire\Component;
 
 class StatusFilters extends Component
 {
-    public $status = 'All';
-
+    public $status;
     public $statusCount;
-
-    public $queryString = [
-        'status',
-    ];
 
     public function mount()
     {
         $this->statusCount = Status::getCount();
+        $this->status = request()->status ?? 'All';
 
         if (Route::currentRouteName() === 'idea.show') {
             $this->status = null;
-            $this->queryString = [];
+            $this->emit('queryStringUpdatedStatus', $this->status);
         }
     }
 
@@ -31,9 +27,10 @@ class StatusFilters extends Component
     {
         $this->status = $newStatus;
 
-        return redirect()->route('idea.index', [
-            'status' => $this->status,
-        ]);
+        //if ($this->getPreviousRouteName() === 'idea.show') {}
+            return redirect()->route('idea.index', [
+                'status' => $this->status,
+            ]);
     }
 
     public function render()
