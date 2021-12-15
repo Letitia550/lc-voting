ea69635aeade318745e63c901a36ad40ebc0a17a
Letitia550 <negotin.letitia@gmail.com>
2021-12-15 13:47:50 +0200

    Episode 39 - Edit Policies

--- resources/views/livewire/idea-show.blade.php
+++ resources/views/livewire/idea-show.blade.php
@@ -38,6 +38,7 @@
                                 @click.away="isOpen = false"
                                 @keydown.escape.window="isOpen = false"
                             >
+                                @can('update', $idea)
                                 <li>
                                     <a
                                         href="#"
@@ -49,6 +50,7 @@
                                         Edit Idea
                                     </a>
                                 </li>
+                                @endcan
                                 <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Idea</a></li>
                                 <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
                             </ul>
